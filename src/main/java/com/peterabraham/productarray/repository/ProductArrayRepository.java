f631a53c0f6f764db9752205568f21065bc54b63
Peter Abraham <peterabrahamdev@gmail.com>
2024-07-09 16:38:26 +0200

    feat: create ProductArrayRepository persistence layer

--- src/main/java/com/peterabraham/productarray/repository/ProductArrayRepository.java
+++ src/main/java/com/peterabraham/productarray/repository/ProductArrayRepository.java
@@ -1,2 +1,12 @@
-package com.peterabraham.productarray.repository;public interface ProductArrayRepository {
+package com.peterabraham.productarray.repository;
+
+import com.peterabraham.productarray.entity.ProductArrayCalculationRecord;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductArrayRepository extends CrudRepository<ProductArrayCalculationRecord, Long> {
+    List<ProductArrayCalculationRecord> findAll();
 }
