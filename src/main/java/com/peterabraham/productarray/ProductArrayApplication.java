22ffaeeb13dc96800120720c3fb764d8c16bf5ea
Peter Abraham <peterabrahamdev@gmail.com>
2024-07-09 11:53:16 +0200

    init: initial commit

---
+++ src/main/java/com/peterabraham/productarray/ProductArrayApplication.java
@@ -0,0 +1,13 @@
+package com.peterabraham.productarray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductArrayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductArrayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/peterabraham/productarray/ProductArrayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peterabraham.productarray;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductArrayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
